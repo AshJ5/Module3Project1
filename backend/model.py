2c13062fac493caf10c468dd53013334f27201a5
Roberto <rduran2004@icloud.com>
2025-11-25 13:30:16 -0600

    update

---
+++ backend/model.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Character(Base):
+    __tablename__ = "characters"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    gender = Column(String)
+    class_ = Column(String)
